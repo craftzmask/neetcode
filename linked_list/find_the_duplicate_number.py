c1cfe3dc804fa0829cff303b73eb1e616849fe6d
Khanh Chung <khanh.d.chung@gmail.com>
2023-03-13 12:44:42 -0700

    Find the Duplicate Number

---
+++ linked_list/find_the_duplicate_number.py
@@ -0,0 +1,13 @@
+class Solution:
+    def findDuplicate(self, nums: List[int]) -> int:
+        slow, fast = nums[0], nums[nums[0]]
+        while slow != fast:
+            slow = nums[slow]
+            fast = nums[nums[fast]]
+        
+        slow2 = 0
+        while slow2 != slow:
+            slow = nums[slow]
+            slow2 = nums[slow2]
+        
+        return slow
